557c0eefd5c9d38e1ad9a88a3276c085d3c38f47
Steven Seifried <gitlab@canox.net>
2023-04-02 20:09:26 +0200

    Update build_latest_stable.sh

--- build_latest_stable.sh
+++ build_latest_stable.sh
@@ -5,6 +5,7 @@ LANGUAGE=C
 RELEASE=$(curl -sL https://api.github.com/repos/Ralim/IronOS/releases/latest | jq -r ".tag_name")
 git clone -c advice.detachedHead=false --recursive --branch $RELEASE https://github.com/Ralim/IronOS.git
 cd IronOS
+systemctl start docker.service
 docker-compose up -d builder
 docker exec ironos-builder-1 bash -c "cd source/source && make -j$(nproc) model=Pinecil custom_multi_langs='DE EN' firmware-multi_compressed_Custom" > /dev/null 2>&1
 docker exec ironos-builder-1 bash -c "cd source/source && make -j$(nproc) model=Pinecilv2 custom_multi_langs='DE EN' firmware-multi_compressed_Custom" > /dev/null 2>&1
